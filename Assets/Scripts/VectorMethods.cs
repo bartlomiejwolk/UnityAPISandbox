a23d45ddbe4e7b10cd1bc816d8bea8ae0d9713d6
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-06-03 19:02:32 +0200

    Visualise Vector3.Cross()

---
+++ Assets/Scripts/VectorMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class VectorMethods : MonoBehaviour
+{
+    [Header("Angle")]
+    [SerializeField]
+    private Transform m_ObjectA;
+
+    [SerializeField]
+    private Transform m_ObjectB;
+
+    [SerializeField]
+    private Transform m_CrossProductPointer;
+
+    void Update()
+    {
+        // Vector3.Angle()
+        {
+            float angle = Vector3.Angle(m_ObjectA.forward, m_ObjectB.forward);
+            Debug.Log(string.Format("Angle between A and B: {0}", angle));
+        }
+
+        // Vector3.Cross()
+        {
+            Vector3 cross = Vector3.Cross(m_ObjectA.forward, m_ObjectB.forward);
+            Debug.Log(string.Format("Cross product between A and B: {0}", cross));
+            Quaternion newRot = new Quaternion();
+            newRot.SetLookRotation(cross, Vector3.up);
+            m_CrossProductPointer.rotation = newRot;
+        }
+    }
+}
